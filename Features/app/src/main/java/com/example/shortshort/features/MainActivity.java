ae5d11ac6a7f5336a6e027c5b61b7126994c785d
Marco Wang <m.aesophor@gmail.com>
2018-12-12 14:26:20 +0800

    today's hit

--- Features/app/src/main/java/com/example/shortshort/features/MainActivity.java
+++ Features/app/src/main/java/com/example/shortshort/features/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
+import android.media.Image;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -37,6 +38,9 @@ import org.opencv.imgproc.Imgproc;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import static org.opencv.core.Core.BORDER_DEFAULT;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -60,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
+        OpenCVLoader.initDebug();
     }
 
     @Override
@@ -120,6 +125,16 @@ public class MainActivity extends AppCompatActivity {
         } else if (id == R.id.CannyEdges) {
             processedView = (ImageView) findViewById(R.id.processedImage);
             canny();
+        } else if (id == R.id.SobelFilter) {
+            dogView1 = (ImageView) findViewById(R.id.dogImage1);
+            dogView2 = (ImageView) findViewById(R.id.dogImage2);
+            processedView = (ImageView) findViewById(R.id.processedImage);
+            sobel();
+            //Mat sobelMat = sobel();
+            //segmentChineseCharacters(sobelMat);
+        } else if (id == R.id.HarrisCorners) {
+            processedView = (ImageView) findViewById(R.id.processedImage);
+            HarrisCorner();
         }
 
         return super.onOptionsItemSelected(item);
@@ -273,6 +288,77 @@ public class MainActivity extends AppCompatActivity {
 
         Utils.matToBitmap(cannyEdges, processedBitmap);
         processedView.setImageBitmap(processedBitmap);
+    }
+
+    public void sobel() {
+        Mat grayMat = new Mat();
+        Mat sobel = new Mat();
+        Mat grad_x = new Mat();
+        Mat abs_grad_x = new Mat();
+        Mat grad_y = new Mat();
+        Mat abs_grad_y = new Mat();
+
+        Imgproc.cvtColor(originalMat, grayMat, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.Sobel(grayMat, grad_x, CvType.CV_16S, 1, 0, 3, 1, 0);
+        Imgproc.Sobel(grayMat, grad_y, CvType.CV_16S, 0, 1, 3, 1, 0);
+
+        Core.convertScaleAbs(grad_x, abs_grad_x);
+        Core.convertScaleAbs(grad_y, abs_grad_y);
+        Core.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 1, sobel);
+
+        processedBitmap = Bitmap.createBitmap(currentBitmap.getWidth(), currentBitmap.getHeight(), currentBitmap.getConfig());
+        Utils.matToBitmap(sobel, processedBitmap);
+        processedView.setImageBitmap(processedBitmap);
+    }
+
+    void segmentChineseCharacters(Mat sobelMat) {
+        Mat bin1 = new Mat(sobelMat.rows(), sobelMat.cols(), CvType.CV_8UC1);
+        double threshold = Imgproc.threshold(sobelMat, bin1, 0, 255, Imgproc.THRESH_OTSU);
+        TextView tv1 = (TextView) findViewById(R.id.textView1);
+        tv1.setText("Threshold value = " + threshold);
+
+        List<MatOfPoint> contourListTemp = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(bin1, contourListTemp, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0]) {
+            MatOfPoint matOfPoint = contourListTemp.get(idx);
+            Rect rect = Imgproc.boundingRect(matOfPoint);
+            Imgproc.rectangle(originalMat, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(100));
+        }
+        Bitmap processedBitmap1 = Bitmap.createBitmap(currentBitmap.getWidth(), currentBitmap.getHeight(), currentBitmap.getConfig());
+        Utils.matToBitmap(bin1, processedBitmap1);
+        dogView1.setImageBitmap(processedBitmap1);
+
+        Bitmap processedBitmap2 = Bitmap.createBitmap(currentBitmap.getWidth(), currentBitmap.getHeight(), currentBitmap.getConfig());
+        Utils.matToBitmap(originalMat, processedBitmap2);
+        dogView2.setImageBitmap(processedBitmap2);
+    }
+
+    public void HarrisCorner() {
+        Mat grayMat = new Mat();
+        Mat corners = new Mat();
+        Imgproc.cvtColor(originalMat, grayMat, Imgproc.COLOR_BGR2GRAY);
+
+        Mat tempDst = new Mat();
+        Imgproc.cornerHarris(grayMat, tempDst, 7, 5, 0.05, BORDER_DEFAULT);
+        Mat tempDstNorm = new Mat();
+        Core.normalize(tempDst, tempDstNorm, 0, 255, Core.NORM_MINMAX);
+        Core.convertScaleAbs(tempDstNorm, corners);
+
+        Random r = new Random();
+        for (int i = 0; i < tempDst.cols(); i++) {
+            for (int j = 0; j < tempDstNorm.rows(); j++) {
+                double[] value = tempDstNorm.get(j, i);
+                if (value[0] > 37) {
+                    Imgproc.circle(originalMat, new Point(i, j), 2, new Scalar(255, 0, 0, 255), 1, 8, 0);
+                }
+            }
+        }
+
+        Bitmap processedImage = Bitmap.createBitmap(originalMat.cols(), originalMat.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(originalMat, processedImage);
+        processedView.setImageBitmap(processedImage);
     }
 
 }
